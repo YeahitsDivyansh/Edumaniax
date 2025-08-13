8e6c2164b0daf1d78c733ed631f6912e4bc44f1d
AnujYelve <anujyelve3074@gmail.com>
2025-08-13 19:39:20 +0530

    notes fully changed

--- client/src/pages/SocialLearningNotes/9-10Section5.jsx
+++ client/src/pages/SocialLearningNotes/9-10Section5.jsx
@@ -12,7 +12,7 @@ const Module5ResponsibleDecisionMaking = ({ topicRefs }) => {
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setVisibleCards([0, 1, 2, 3, 4, 5, 6, 7]);
+      setVisibleCards([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]);
     }, 300);
     return () => clearTimeout(timer);
   }, []);
